4adc5f9a665dc9aa8c421444610525e0af4226c9
Selin Birgul <selinece4880@gmail.com>
2025-08-22 10:11:25 -0600

    Initial commit

---
+++ src/main/java/edu/carroll/cs341_demo/Cs341Application.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs341_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs341Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs341Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/carroll/cs341_demo/Cs341ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs341_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs341ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
