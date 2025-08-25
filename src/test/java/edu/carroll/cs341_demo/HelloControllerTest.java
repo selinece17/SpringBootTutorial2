bf648354fe2f8510927ab5ac19433d847c41cb1c
Selin Birgul <selinece4880@gmail.com>
2025-08-25 10:48:17 -0600

    added a test class for controller

---
+++ src/test/java/edu/carroll/cs341_demo/HelloControllerTest.java
@@ -0,0 +1,25 @@
+package edu.carroll.cs341_demo;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void indexTest() throws Exception {
+        mockMvc.perform(get("/hello")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello from Spring Boot!")));
+    }
+}
