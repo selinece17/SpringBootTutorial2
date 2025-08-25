dffe38303ba73de1756936e3fc61e5551021dc67
Selin Birgul <selinece4880@gmail.com>
2025-08-25 11:30:51 -0600

    worked on dynamic html rendering

--- src/main/java/edu/carroll/cs341_demo/HelloController.java
+++ src/main/java/edu/carroll/cs341_demo/HelloController.java
@@ -1,11 +1,15 @@
 package edu.carroll.cs341_demo;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String index() {
+    @GetMapping("/")
+    public String index(@RequestParam(value="name", required=false, defaultValue="Student")String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
