2221d7b20e1303c0a198e22c415cec01a39b6985
Selin Birgul <selinece4880@gmail.com>
2025-08-26 22:33:50 -0600

    added IndexController and its unit test

---
+++ src/test/java/edu/carroll/cs341_demo/controller/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package edu.carroll.cs341_demo.controller;
+
+import static org.hamcrest.Matchers.containsString;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(IndexController.class)
+public class IndexControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void indexTest() throws Exception {
+        mockMvc.perform(get("/")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Welcome to the application!")))
+                .andExpect(content().string(containsString("login")));
+    }
+}
